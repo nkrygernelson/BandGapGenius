a3e20ba89dec09a482ee0283bd891cbb3a906b6f
nkrygernelson <s215196@dtu.dk>
2024-06-12 13:43:16 +0200

    Gets Data from MP, creates attributes, trains with NN and predicts.

--- get_data.py
+++ get_data.py
@@ -1,4 +1,5 @@
 from mp_api.client import MPRester
+import pymatgen
 api_key = 'GPPXFjpPCxMe6U8Uz9CQa4TEhNbaaPCY'
 
 
@@ -12,7 +13,18 @@ def get_ternary_semiconductors():
     with MPRester(api_key) as mpr:
         docs = mpr.materials.summary.search(num_elements=3, band_gap = (0.3,3),fields=['band_gap','structure']  )
         for i in range(len(docs)):
-            write_to_text_file(str(docs[i].structure.reduced_formula) + ","+ str(docs[i].band_gap) + "\n")
+            #temporary removal of elements containg "("
+            if "(" not in str(docs[i].structure.reduced_formula):
+                write_to_text_file(str(docs[i].structure.reduced_formula) + ","+ str(docs[i].band_gap) + "\n")
 
-
-get_ternary_semiconductors()
+def refined_query():
+    include_list = ["P", "S"]
+    exclude_list = ["H", "O", "N", "F", "I", "Br", "Cl", "C", "Se", "Si", "As"]
+    with MPRester(api_key) as mpr:
+        docs = mpr.materials.summary.search(num_elements= 3,band_gap=(0.1,6),energy_above_hull=(0,0.2), fields=['band_gap','structure'],  )
+        for i in range(len(docs)):
+            elements = pymatgen.core.composition.Composition(docs[i].structure.reduced_formula).get_el_amt_dict().keys()
+            #we only write the compunds that contain the elements in the include list and do not contain the elements in the exclude list
+            if all(elem in elements for elem in include_list) and not any(elem in elements for elem in exclude_list):
+                write_to_text_file(str(docs[i].structure.reduced_formula) + ","+ str(docs[i].band_gap) + "\n")
+refined_query()
